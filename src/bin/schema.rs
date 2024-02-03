1c849f1dc5ba22288abe38009cb86d11547f957e
Joey-mi <joanne.millard@ucalgary.ca>
2024-02-02 22:19:02 -0700

    Make smart contract template

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cosm_wasm::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
