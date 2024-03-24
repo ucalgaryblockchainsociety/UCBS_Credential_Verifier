75361bcfca5e129170d8933ba3f1cf28b3cff875
dyenaan <91904892+dyenaan@users.noreply.github.com>
2024-03-24 14:36:33 -0600

    Delete src directory

--- src/state.rs
+++
@@ -1,9 +0,0 @@
-use cw_storage_plus::{Map, Item};
-use cosmwasm_std::Addr;
-use crate::msg::UserInfo;
-
-
-//users request, maps requestID to request status
-pub const USER_REQUEST: Map<String, UserInfo> = Map::new("requests");
-pub const EMP_REQUESTS: Map<String, Vec<String>> = Map::new("employersrequests");
-pub const OWNER: Item<Addr> = Item::new("owner");
